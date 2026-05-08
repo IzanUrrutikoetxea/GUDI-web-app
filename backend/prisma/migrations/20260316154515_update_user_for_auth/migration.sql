4d6a387027eeb408efeb51c115cd08f3611c7c08
gorkitix <gorkaa623@gmail.com>
2026-05-08 13:25:42 +0200

    Update migration.sql (#10)

--- backend/prisma/migrations/20260316154515_update_user_for_auth/migration.sql
+++ backend/prisma/migrations/20260316154515_update_user_for_auth/migration.sql
@@ -4,6 +4,7 @@
   - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
   - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
 
+
 */
 -- CreateEnum
 CREATE TYPE "UserRole" AS ENUM ('ADMIN', 'USER');
